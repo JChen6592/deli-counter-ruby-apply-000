3be1b808091cfc9678d84411f518374943466f68
John Chen <johnchen062094@gmail.com>
2020-04-26 21:58:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,11 @@
 katz_deli = []
 
 def line(line_status)
-  if line_status 
+  if line_status.length == 0 
+    puts "The line is currently empty."
+  else 
+    counter = 0 
+    until counter == line_status.length 
 end
     
     
