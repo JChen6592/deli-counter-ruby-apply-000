17ee44ba1115d9dbd309094908c65b4cbc9aa3ba
John Chen <johnchen062094@gmail.com>
2020-04-26 21:56:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(numinline)
       line_method_array.push("#{index}. #{name}")
     end
     output = line_method_arry.join(" ")
-    puts "The line is currently: #{line_method_array.join(" ")}"
+    puts "The line is currently: #{output}"
   end
 end
     
